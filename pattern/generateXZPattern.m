cf3c63a2468efc020caf9866828fae2bcdfe4a74
MyYo <MyYo@users.noreply.github.com>
2024-06-26 17:04:06 -0700

    Make sure photobleach dots show up

--- pattern/generateXZPattern.m
+++ pattern/generateXZPattern.m
@@ -35,12 +35,17 @@ y_end_mm   = [y_end_mm    hLinePositions*base+hLineBias];
 %% Add dots
 number_of_dots = 10;
 rng(13); % Seed
-rand_numbers_m1to1 = (2*rand(2, number_of_dots)-1); % Random numbers between -1 and 1
-rand_numbers_m1to1 = round(rand_numbers_m1to1*4)/4; % Only specific cells are allowed
-dot_xy_mm = rand_numbers_m1to1*0.1;
+tmp1 = (2*rand(2, number_of_dots*2)-1); % Random numbers between -1 and 1
+tmp1 = round(tmp1*4)/4; % Only specific cells are allowed
+tmp2 = unique(tmp1(1,:)+1i*tmp1(2,:));
+rand_numbers_m1to1(1,:) = real(tmp2(1:number_of_dots));
+rand_numbers_m1to1(2,:) = imag(tmp2(1:number_of_dots));
 
-x_start_mm = [x_start_mm (dot_xy_mm(1,:)-1e-3)];
-x_end_mm = [x_end_mm (dot_xy_mm(1,:)+1e-3)];
+dot_xy_mm = rand_numbers_m1to1*0.1+5e-3;
+
+d_dot_mm = 6e-3;
+x_start_mm = [x_start_mm (dot_xy_mm(1,:)-d_dot_mm)];
+x_end_mm = [x_end_mm (dot_xy_mm(1,:)+d_dot_mm)];
 y_start_mm = [y_start_mm dot_xy_mm(2,:)];
 y_end_mm = [y_end_mm dot_xy_mm(2,:)];
 
