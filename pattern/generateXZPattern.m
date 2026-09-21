3fb8b8d8df0892cb45b7e6e47eb6176643c90174
zerlinazl <76027329+zerlinazl@users.noreply.github.com>
2026-09-21 16:12:55 -0700

    reorganize for readability

--- pattern/generateXZPattern.m
+++ pattern/generateXZPattern.m
@@ -1,12 +1,11 @@
 function [xStart_mm, xEnd_mm, yStart_mm, yEnd_mm, z_mm] = generateXZPattern()
 % Generates the instructions (lines) for XY lines.
-% Lines go from (xStart_mm(i), yStart_mm(i)) to (xEnd_mm(i), yEnd_mm(i)) at depth z.
+% Constructors:
+%   VerticalLine(x, yStart, yEnd, depth)
+%   DiagonalLine(xStart, xEnd, yStart, yEnd, depth)
 
 %% Build line objects
 allLines = {};
-
-% VerticalLine(x, yStart, yEnd, depth)
-% DiagonalLine(xStart, xEnd, yStart, yEnd, depth)
 
 allLines{end+1} = VerticalLine(-0.9, 0.5, -0.5, 65e-3);
 allLines{end+1} = DiagonalLine(-0.88, -0.77, -0.5, 0.5, 65e-3);
@@ -49,34 +48,13 @@ allLines{end+1} = DiagonalLine(-1, 1.7, 0.5, 0.5, 65e-3);
 %% Convert to arrays
 [xStart_mm, xEnd_mm, yStart_mm, yEnd_mm, z_mm] = linesToArrays(allLines);
 
-%% Plot
-figure(1); clf;
-hold on;
-for ii = 1:numel(allLines)
-    plot([xStart_mm(ii), xEnd_mm(ii)], [yStart_mm(ii), yEnd_mm(ii)]);
+%% Preview
+plotPattern(allLines, xStart_mm, xEnd_mm, yStart_mm, yEnd_mm);
 
-    % Midpoint of each line for the label
-    x_mid = (xStart_mm(ii) + xEnd_mm(ii)) / 2;
-    y_mid = (yStart_mm(ii) + yEnd_mm(ii)) / 2;
-    text(x_mid, y_mid, sprintf('%d', ii-1), ...
-        'HorizontalAlignment', 'center', ...
-        'VerticalAlignment', 'middle', ...
-        'FontSize', 8, ...
-        'FontWeight', 'bold');
-end
-xRange = [-0.525 0.875];
-yRange = [-0.175 0.175];
-
-plot(xRange([1 2 2 1 1]), ...
-     yRange([1 1 2 2 1]), ...
-     'k-', 'LineWidth', 2);
-
-hold off
-axis ij
-axis equal
 end
 
 function [xStart_mm, xEnd_mm, yStart_mm, yEnd_mm, z_mm] = linesToArrays(lines)
+% Unpack line objects into parallel coordinate arrays.
 
 n = numel(lines);
 
@@ -87,11 +65,43 @@ yEnd_mm   = zeros(1, n);
 z_mm      = zeros(1, n);
 
 for ii = 1:n
-    xStart_mm(ii) = lines{ii}.xStart_mm;  
+    xStart_mm(ii) = lines{ii}.xStart_mm;
     xEnd_mm(ii)   = lines{ii}.xEnd_mm;
     yStart_mm(ii) = lines{ii}.yStart_mm;
     yEnd_mm(ii)   = lines{ii}.yEnd_mm;
     z_mm(ii)      = lines{ii}.z_mm;
 end
+
+end
+
+function plotPattern(allLines, xStart_mm, xEnd_mm, yStart_mm, yEnd_mm)
+%Draw lines with index labels and the OCT scan area outlined.
+
+% ROI box overlaid on the pattern
+oct_xRange = [-0.525 0.875];
+oct_yRange = [-0.175 0.175];
+
+figure(1); clf;
+hold on;
+
+for ii = 1:numel(allLines)
+    plot([xStart_mm(ii), xEnd_mm(ii)], [yStart_mm(ii), yEnd_mm(ii)]);
+
+    x_mid = (xStart_mm(ii) + xEnd_mm(ii)) / 2;
+    y_mid = (yStart_mm(ii) + yEnd_mm(ii)) / 2;
+    text(x_mid, y_mid, sprintf('%d', ii - 1), ...
+        'HorizontalAlignment', 'center', ...
+        'VerticalAlignment', 'middle', ...
+        'FontSize', 8, ...
+        'FontWeight', 'bold');
+end
+
+plot(oct_xRange([1 2 2 1 1]), ...
+     oct_yRange([1 1 2 2 1]), ...
+     'k-', 'LineWidth', 2);
+
+hold off;
+axis ij;
+axis equal;
 
 end
